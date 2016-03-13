e2b1b895ffa15d03fa27aeb0c226fd41fdd6f98b
Peter Pham <peter.pham00@gmail.com>
2016-03-13 15:01:36 -0400

    Added write to local DB

--- combo_creator/send_form_email.php
+++ combo_creator/send_form_email.php
@@ -1,7 +1,33 @@
 <?php
-  // the message
-  $msg = $_POST['combos'];
+  // // the message
+  // $msg = $_POST['combos'];
 
-  // send email
-  mail("titan.maclub@gmail.com","My subject",$msg);
+  $combo     = $_POST['combos'];
+  $combo      = mysql_real_escape_string($combo);
+
+  // // send email
+  // mail("titan.maclub@gmail.com","My subject",$msg);
+
+	$servername = "localhost";
+	$username = "root";
+	$password = "headkick";
+	$dbname = "titan";
+
+	// Create connection
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	// Check connection
+	if ($conn->connect_error) {
+	    die("Connection failed: " . $conn->connect_error);
+	} 
+
+	$sql = "INSERT INTO combos (march)
+	VALUES ('$combo ')";
+
+	if ($conn->query($sql) === TRUE) {
+	    echo "New record created successfully";
+	} else {
+	    echo "Error: " . $sql . "<br>" . $conn->error;
+	}
+
+	$conn->close();
 ?>
